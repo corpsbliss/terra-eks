3e860f0ee0d816675667a4368012d0b220e42297
root <rathore123rohit@gmail.com>
2025-06-14 21:07:24 -0400

    resources added

--- eks/locals.tf
+++ eks/locals.tf
@@ -1,8 +1,8 @@
 locals {
-    env = "development"
-    region = "us-east-2"
-    zone1 = "us-east-2a"
-    zone2 = "us-east-2b"
-    eks_name = "whiteboard-eks"
-    eks_version = "1.30"
+  env         = "development"
+  region      = "us-east-2"
+  zone1       = "us-east-2a"
+  zone2       = "us-east-2b"
+  eks_name    = "whiteboard-eks"
+  eks_version = "1.30"
 }
